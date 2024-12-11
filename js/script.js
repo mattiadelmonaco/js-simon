ad13caf069a90becfa04df30e780627aa2515866
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-11 16:15:20 +0100

    feat: add random number in html's div

--- js/script.js
+++ js/script.js
@@ -15,22 +15,43 @@ function getRandomNumber(min, max) {
 // DOM ELEMENTS 
 
 const counterElm = document.getElementById("counter")
-const randomNumberElm = document.getElementById("random-number")
+const randomNumberElm = document.querySelectorAll(".random-number")
 const numberFieldElm = document.getElementById("number-field")
 const confirmElm = document.getElementById("confirm")
 const numberGuessElm = document.getElementById("number-guess")
+const randomNumberContainer = document.getElementById("random-number-container")
 
-// VARIABLES
+// GLOBAL VARIABLES
 
 let countdown = 3
+let randomNumberArray = []
 
 // LOGIC 
+
+// countdown and functions
 
 const endCountdown = setInterval(() => {
     if (countdown === 0) {
         clearInterval(endCountdown)
+        // randomNumberContainer.classList.add ("d-none")
     } 
     counterElm.innerHTML = countdown
     countdown--
 }, 1000)
+
+// cicle to get 5 random numbers and put to an array
+
+for (i = 0; i < randomNumberElm.length; i++) {
+    let randomNumber = getRandomNumber(1, 99)
+    randomNumberArray.push(randomNumber)
+    randomNumberElm.innerHTML = randomNumberArray
+}
+
+// cicle to inner in html the random number in array
+
+for (i = 0; i < randomNumberElm.length; i++) {
+    randomNumberElm[i].innerHTML = randomNumberArray[i]
+}
+console.log(randomNumberElm)
+
     
